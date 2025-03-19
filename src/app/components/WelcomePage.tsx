489953e5e74df1e5dd1c6284dc54ab1e94498991
Jason Achkar Diab <ja_achka@live.concordia.ca>
2025-03-18 22:41:24 -0400

    Remove .cursorrules file and several SVG assets; refactor Home component to use WelcomePage component instead of inline content.

---
+++ src/app/components/WelcomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the App</h1>
+            <p>This is the welcome page</p>
+        </div>
+    )
+}
+
+export default WelcomePage;
